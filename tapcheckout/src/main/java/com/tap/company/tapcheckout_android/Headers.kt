2bfff2bd7c4654faa8ed13f2bcf8637e0a9f3f0f
AhlaamK-tap <57221514+AhlaamK-tap@users.noreply.github.com>
2025-03-13 12:07:00 +0300

    adding files in progress

--- tapcheckout/src/main/java/com/tap/company/tapcheckout_android/Headers.kt
+++
@@ -1,7 +0,0 @@
-package com.tap.company.tapcheckout_android
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Headers(var mdn: String?=null, var application: String?=null) : Parcelable
