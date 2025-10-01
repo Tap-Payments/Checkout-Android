60264ac84ac4fdcd7ae77ab767682b7893e1e6c8
AhlaamK-tap <57221514+AhlaamK-tap@users.noreply.github.com>
2025-10-01 13:40:38 +0300

    version update

--- tapcheckoutsdk/src/main/java/company/tap/tapcheckout_android/scanner_activity/ScannerActivity.kt
+++ tapcheckoutsdk/src/main/java/company/tap/tapcheckout_android/scanner_activity/ScannerActivity.kt
@@ -1,12 +1,15 @@
 package company.tap.tapcheckout_android.scanner_activity
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.webkit.WebView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import company.tap.cardscanner.CameraFragment
 import company.tap.cardscanner.TapCard
 import company.tap.cardscanner.TapScannerCallback
@@ -17,8 +20,9 @@ import company.tap.tapcheckout_android.TapCheckout
 
 
 private const val SCAN_CARD_ID = 101
+private const val CAMERA_PERMISSION_REQUEST_CODE = 200
 
-class ScannerActivity : AppCompatActivity(), TapTextRecognitionCallBack, TapScannerCallback{
+class ScannerActivity : AppCompatActivity(), TapTextRecognitionCallBack, TapScannerCallback {
     //InlineViewCallback {
     lateinit var webView: WebView
     private var textRecognitionML: TapTextRecognitionML? = null
@@ -37,16 +41,16 @@ class ScannerActivity : AppCompatActivity(), TapTextRecognitionCallBack, TapScan
 //            CameraActivity::class.java
 //        )
 //        startActivity(chooseImageIntent)
-      //  val intent = ScanCardIntent.Builder(this).build()
-      //  startActivityForResult(intent, SCAN_CARD_ID)
+        //  val intent = ScanCardIntent.Builder(this).build()
+        //  startActivityForResult(intent, SCAN_CARD_ID)
+        if (isCameraPermissionGranted()) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.inline_container, CameraFragment())
+                .commit()
 
-        val cameraFragment = CameraFragment()
-        cameraFragment.setCallBack(this,this)
+        }else  requestCameraPermission()
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.inline_container,cameraFragment)
-            .commit()
 //        supportFragmentManager
 //            .beginTransaction()
 //            .replace(company.tap.tapcardformkit.R.id.inline_container, CameraFragment())
@@ -65,12 +69,12 @@ class ScannerActivity : AppCompatActivity(), TapTextRecognitionCallBack, TapScan
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
             SCAN_CARD_ID -> if (resultCode == Activity.RESULT_OK) {
-               /* val card = data!!.getParcelableExtra<Card>(ScanCardIntent.RESULT_PAYCARDS_CARD)
-                if (card != null) {
-                    fillCardNumber(cardNumber = card?.cardNumber.toString(), cardHolderName =card?.cardHolderName ?: "" , cvv ="" , expiryDate =card?.expirationDate ?: "" )
-                    setResult(Activity.RESULT_OK, data)
-                    finish()
-                }*/
+                /* val card = data!!.getParcelableExtra<Card>(ScanCardIntent.RESULT_PAYCARDS_CARD)
+                 if (card != null) {
+                     fillCardNumber(cardNumber = card?.cardNumber.toString(), cardHolderName =card?.cardHolderName ?: "" , cvv ="" , expiryDate =card?.expirationDate ?: "" )
+                     setResult(Activity.RESULT_OK, data)
+                     finish()
+                 }*/
             }
             else -> super.onActivityResult(requestCode, resultCode, data)
         }
@@ -81,7 +85,7 @@ class ScannerActivity : AppCompatActivity(), TapTextRecognitionCallBack, TapScan
     override fun onRecognitionSuccess(card: TapCard?) {
         println("cardNumber>>>>"+card)
         Toast.makeText(this@ScannerActivity, card?.cardNumber.toString(), Toast.LENGTH_LONG).show()
-      //  if (card?.cardNumber != null && card.cardHolder != null && card.expirationDate != null) {
+        //  if (card?.cardNumber != null && card.cardHolder != null && card.expirationDate != null) {
         if (card?.cardNumber != null ) {
             TapCheckout.fillCardNumber(
                 cardNumber = card?.cardNumber.toString(),
@@ -89,7 +93,7 @@ class ScannerActivity : AppCompatActivity(), TapTextRecognitionCallBack, TapScan
                 cvv = "",
                 expiryDate = card.expirationDate ?: ""
             )
-          //  incrementalCount = 0
+            //  incrementalCount = 0
         }
     }
 
@@ -119,18 +123,56 @@ class ScannerActivity : AppCompatActivity(), TapTextRecognitionCallBack, TapScan
 
     }
 
-  /*  override fun onScanCardFailed(e: Exception?) {
-        Toast.makeText(this@ScannerActivity, e?.message.toString(), Toast.LENGTH_LONG).show()
-    }
+    /*  override fun onScanCardFailed(e: Exception?) {
+          Toast.makeText(this@ScannerActivity, e?.message.toString(), Toast.LENGTH_LONG).show()
+      }
 
-    override fun onScanCardFinished(card: Card?, cardImage: ByteArray?) {
-        Toast.makeText(this@ScannerActivity, card?.cardNumber.toString(), Toast.LENGTH_LONG).show()
+      override fun onScanCardFinished(card: Card?, cardImage: ByteArray?) {
+          Toast.makeText(this@ScannerActivity, card?.cardNumber.toString(), Toast.LENGTH_LONG).show()
 
-    }*/
+      }*/
     override fun onBackPressed() {
         super.onBackPressed()
         finish()
 
     }
+    private fun isCameraPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun openCameraFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.inline_container, CameraFragment())
+            .commit()
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCameraFragment()
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+
 
 }
